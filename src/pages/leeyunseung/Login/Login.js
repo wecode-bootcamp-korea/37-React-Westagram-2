18db141f5c4de34d9efeaab1e99ae659b32afdf8
1211lys <hpppower111@gmail.com>
2022-09-01 15:11:40 +0900

    Add : Mission 1 - 사용자 입력 데이터 저장 기능 구현

--- src/pages/leeyunseung/Login/Login.js
+++ src/pages/leeyunseung/Login/Login.js
@@ -1,7 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
+import "./Login.scss";
 
 function LoginLee() {
-  return <h1>Hello world</h1>;
+  let [userId, SetUserId] = useState("");
+  let [userPassWord, setUserPassWord] = useState("");
+  const onChangeId = (event) => {
+    SetUserId(event.target.value);
+  };
+
+  const onChangePassWord = (event) => {
+    setUserPassWord(event.target.value);
+  };
+
+  return (
+    <div className="loginWrapper">
+      <div>
+        <h1>Westargram</h1>
+        <input
+          placeholder="전화번호, 사용자 이름 또는 이메일"
+          type="text"
+          disabled=""
+          value={userId}
+          onChange={onChangeId}
+        />
+        <input
+          placeholder="비밀번호"
+          type="password"
+          disabled=""
+          value={userPassWord}
+          onChange={onChangePassWord}
+        />
+        <button type="button" disabled="">
+          로그인
+        </button>
+        <a href="https://www.naver.com">비밀번호를 잊으셨나요?</a>
+      </div>
+    </div>
+  );
 }
 
 export default LoginLee;
