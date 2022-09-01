c5eb63e472b5c2b153000148ab7ca2cdf8eb2b28
Hyerin Joo <candy980426@gmail.com>
2022-09-01 12:27:57 +0900

    Add : Login페이지 jsx로 수정 및 스타일링

--- src/pages/joohyerin/Login/Login.js
+++ src/pages/joohyerin/Login/Login.js
@@ -1,7 +1,39 @@
 import React from "react";
+import "../Login/Login.scss";
+import { useNavigate } from "react-router-dom";
 
-function LoginJoo() {
-  return <h1>Hello world</h1>;
-}
+const LoginJoo = () => {
+  const navigate = useNavigate();
+
+  return (
+    <div className="login">
+      <div className="box-all">
+        <h1 className="text-title">Westagram</h1>
+        <form className="form-login">
+          <input
+            id="input-id"
+            type="text"
+            placeholder="전화번호, 사용자 이름 또는 이메일"
+          />
+          <input id="input-pw" type="password" placeholder="비밀번호" />
+          <button
+            onClick={() => {
+              navigate("/main");
+            }}
+            id="btn-login"
+            type="submit"
+          >
+            로그인
+          </button>
+        </form>
+        <p className="message-emailError">*이메일 형식을 다시 확인해주세요.</p>
+        <p className="message-pwError">
+          *비밀번호가 5글자 이상인지 확인해주세요.
+        </p>
+        <p className="message-forgetPw">비밀번호를 잊으셨나요?</p>
+      </div>
+    </div>
+  );
+};
 
 export default LoginJoo;
