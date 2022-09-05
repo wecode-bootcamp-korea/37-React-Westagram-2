bcbebf94602c3238db6611e58b8ba0f745f95443
1211lys <hpppower111@gmail.com>
2022-09-05 19:08:14 +0900

    ADD : Nav component 추가

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,7 +1,24 @@
 import React from "react";
+import "./Nav.scss";
 
 function Nav() {
-  return <h1>Hello world</h1>;
+  return (
+    <div className="navbar">
+      <div className="nav-left">
+        <img src="/images/Nav/logo.png" alt="icon" />
+        <p>Wesartgram</p>
+      </div>
+      <div className="nav-center">
+        <img src="/images/Nav/navsearch.png" alt="icon" />
+        <input className="navbar-input" type="text" placeholder="검색" />
+      </div>
+      <div className="nav-right">
+        <img src="/images/Nav/search.png" alt="icon" />
+        <img src="/images/Nav/heart.png" alt="icon" />
+        <img src="/images/Nav/mypage.png" alt="icon" />
+      </div>
+    </div>
+  );
 }
 
 export default Nav;
