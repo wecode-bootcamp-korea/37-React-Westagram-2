6bada569ffd205056c73cd0ffabcdf0062f3f08e
smash009 <smash009@naver.com>
2022-09-06 11:00:49 +0900

    MODIFY: 상수데이터 활용 구현

--- src/pages/namjanghyun/Main/components/Aside.js
+++ src/pages/namjanghyun/Main/components/Aside.js
@@ -1,6 +1,10 @@
 import React from "react";
+import { WESTA_INFO_LIST_LI } from "./uiData";
 
 function Aside() {
+  const westaInfoList = WESTA_INFO_LIST_LI.map((infoList) => {
+    return <li key={infoList.id}>{infoList.menuName}</li>;
+  });
   return (
     <aside className="main-right">
       <div className="userInfo">
@@ -101,16 +105,7 @@ function Aside() {
       </div>
 
       <div className="westaInfo">
-        {/* <p>
-          Westagram 정보 &middot; 지원홍보 센터 &middot; API &middot; 채용정보
-          &middot; 개인정보처리방침 &middot; 약관 &middot; 디렉터리 &middot;
-          프로필 &middot; 해시태그 &middot; 언어
-        </p> */}
-        <ul>
-          {WESTA_INFO_LIST_LI.map((infoList) => {
-            return <li key={infoList.id}>{infoList.menuName}</li>;
-          })}
-        </ul>
+        <ul>{westaInfoList}</ul>
         <span>© 2022 Westagram</span>
       </div>
     </aside>
@@ -118,16 +113,3 @@ function Aside() {
 }
 
 export default Aside;
-
-const WESTA_INFO_LIST_LI = [
-  { id: 1, menuName: "Westagram 정보" },
-  { id: 2, menuName: "지원홍보 센터" },
-  { id: 3, menuName: "API" },
-  { id: 4, menuName: "채용정보" },
-  { id: 5, menuName: "개인정보처리방침" },
-  { id: 6, menuName: "약관" },
-  { id: 7, menuName: "디렉터리" },
-  { id: 8, menuName: "프로필" },
-  { id: 9, menuName: "해시태그" },
-  { id: 10, menuName: "언어" },
-];
---
+++ src/pages/namjanghyun/Main/components/uiData.js
@@ -0,0 +1,12 @@
+export const WESTA_INFO_LIST_LI = [
+  { id: 1, menuName: "Westagram 정보" },
+  { id: 2, menuName: "지원홍보 센터" },
+  { id: 3, menuName: "API" },
+  { id: 4, menuName: "채용정보" },
+  { id: 5, menuName: "개인정보처리방침" },
+  { id: 6, menuName: "약관" },
+  { id: 7, menuName: "디렉터리" },
+  { id: 8, menuName: "프로필" },
+  { id: 9, menuName: "해시태그" },
+  { id: 10, menuName: "언어" },
+];
