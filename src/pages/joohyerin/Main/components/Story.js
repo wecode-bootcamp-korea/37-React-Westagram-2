8e647ce0e4ac3d89a24419e2231c29453765489b
Hyerin Joo <candy980426@gmail.com>
2022-09-06 16:11:49 +0900

    Refactor : Story컴포넌트에 사용자 정보 상수데이터로 선언 및 map함수 적용

--- src/pages/joohyerin/Main/components/Story.js
+++ src/pages/joohyerin/Main/components/Story.js
@@ -3,24 +3,24 @@ import React from "react";
 const Story = () => {
   return (
     <div className="box-story">
-      <li>
-        <img
-          className="img-profile-56px img-story"
-          src={`${process.env.PUBLIC_URL}/images/joohyerin/profile_apple.jpg`}
-          alt="프로필 이미지"
-        />
-        <p className="name-story">apple_01</p>
-      </li>
-      <li>
-        <img
-          className="img-profile-56px img-story"
-          src={`${process.env.PUBLIC_URL}/images/joohyerin/profile_peach.jpg`}
-          alt="프로필 이미지"
-        />
-        <p className="name-story">peach_02</p>
-      </li>
+      {userInfoList.map((userInfo) => {
+        return (
+          <li key={userInfo.id}>
+            <img
+              className="img-profile-56px img-story"
+              src={`${process.env.PUBLIC_URL}/images/joohyerin/${userInfo.imgSrc}`}
+              alt="프로필 이미지"
+            />
+            <p className="name-story">{userInfo.userId}</p>
+          </li>
+        );
+      })}
     </div>
   );
 };
-
 export default Story;
+
+const userInfoList = [
+  { id: 1, userId: "apple_01", imgSrc: "profile_apple.jpg" },
+  { id: 2, userId: "peach_02", imgSrc: "profile_peach.jpg" },
+];
