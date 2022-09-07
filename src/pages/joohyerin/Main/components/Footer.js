beeff14f67ddd8edf268a913ef9618ce165b3191
Hyerin Joo <candy980426@gmail.com>
2022-09-07 13:08:50 +0900

    Refactor: Footer목록 상수데이터 파일로 분리

--- src/pages/joohyerin/Main/components/Footer.js
+++ src/pages/joohyerin/Main/components/Footer.js
@@ -1,20 +1,18 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { FOOTER_INFO } from "./FooterData";
 
 const Footer = () => {
   return (
     <div className="footer">
-      <div className="footer-info">
-        <Link to="#">소개 ·</Link>
-        <Link to="#">도움말 ·</Link>
-        <Link to="#">홍보센터 ·</Link>
-        <Link to="#">API ·</Link>
-        <Link to="#">채용 정보 ·</Link>
-        <Link to="#">개인정보처리방침 ·</Link>
-        <Link to="#">약관 ·</Link>
-        <Link to="#">위치 ·</Link>
-        <Link to="#">언어</Link>
-      </div>
+      <ul className="footer-info-list">
+        {FOOTER_INFO.map((info) => {
+          return (
+            <li className="footer-info" key={info.id}>
+              {info.title} ·
+            </li>
+          );
+        })}
+      </ul>
       <p>©2022 WESTAGRAM FROM HYERIN</p>
     </div>
   );
