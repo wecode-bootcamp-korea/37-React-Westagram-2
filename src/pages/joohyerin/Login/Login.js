348a8926dc8cf087e1a1b77fee6165b410b075c6
Hyerin Joo <candy980426@gmail.com>
2022-09-07 21:43:51 +0900

    Refactor: 리팩토링 및 로그인&회원가입 구현

--- src/pages/joohyerin/Login/Login.js
+++ src/pages/joohyerin/Login/Login.js
@@ -3,10 +3,10 @@ import "../Login/Login.scss";
 import { useNavigate } from "react-router-dom";
 
 const LoginJoo = () => {
-  const navigate = useNavigate();
   const [userId, setUserId] = useState("");
   const [userPw, setUserPw] = useState("");
   const [disabled, setDisabled] = useState(true);
+  const navigate = useNavigate();
   const saveUserId = (event) => {
     setUserId(event.target.value);
   };
@@ -14,73 +14,77 @@ const LoginJoo = () => {
     setUserPw(event.target.value);
   };
   const handleDisabled = () => {
-    if (userId.includes("@") && userPw.length > 4) {
-      setDisabled(false);
-    }
+    let validation = userId.includes("@") && userPw.length > 4;
+    return validation ? setDisabled(false) : setDisabled(true);
+  };
+
+  const clickLogin = (e) => {
+    e.preventDefault();
+    fetch("http://10.58.3.201:3000/auth/signin", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json;charset=utf-8",
+      },
+      body: JSON.stringify({
+        email: userId,
+        password: userPw,
+      }),
+    })
+      .then((response) => {
+        if (response.ok === true) {
+          return response.json();
+        }
+        throw new Error("에러 발생!");
+      })
+      .catch((error) => {
+        alert(error);
+      })
+      .then((data) => {
+        if (data.message === "login success") {
+          localStorage.setItem("TOKEN", data.accessToken);
+          alert("로그인 성공");
+          navigate("/main-joo");
+        } else {
+          alert("로그인 실패");
+        }
+      });
   };
 
   return (
     <div className="login">
-      <div className="box-all">
-        <h1 className="text-title">Westagram</h1>
+      <div className="login-box">
+        <h1 className="login-title">Westagram</h1>
         <form className="form-login">
           <input
+            value={userId}
+            className="login-id"
             onChange={saveUserId}
             onKeyUp={handleDisabled}
-            value={userId}
-            id="input-id"
             type="text"
             placeholder="전화번호, 사용자 이름 또는 이메일"
           />
           <input
+            value={userPw}
+            className="login-pw"
             onChange={saveUserPw}
             onKeyUp={handleDisabled}
-            value={userPw}
-            id="input-pw"
             type="password"
             placeholder="비밀번호"
           />
-          {disabled === true ? (
-            <button
-              disabled={disabled}
-              onClick={() => {
-                navigate("/main-joo");
-              }}
-              id="btn-login"
-              type="submit"
-            >
-              로그인
-            </button>
-          ) : (
-            <button
-              style={{ backgroundColor: "rgb(0, 149, 246, 1)" }}
-              className="btn-hoverEffect"
-              disabled={disabled}
-              onClick={() => {
-                navigate("/main-joo");
-              }}
-              id="btn-login"
-              type="submit"
-            >
-              로그인
-            </button>
-          )}
-          {/* <button
+          <button
+            style={
+              disabled === true
+                ? { backgroundColor: "rgb(0, 149, 246, 0.3)" }
+                : { backgroundColor: "rgb(0, 149, 246, 1)" }
+            }
+            className="login-btn"
             disabled={disabled}
-            onClick={() => {
-              navigate("/main-joo");
-            }}
-            id="btn-login"
-            type="submit"
+            onClick={clickLogin}
           >
             로그인
-          </button> */}
+          </button>
         </form>
-        <p className="message-emailError">*이메일 형식을 다시 확인해주세요.</p>
-        <p className="message-pwError">
-          *비밀번호가 5글자 이상인지 확인해주세요.
-        </p>
-        <p className="message-forgetPw">비밀번호를 잊으셨나요?</p>
+        <p className="login-findPw">비밀번호를 잊으셨나요?</p>
       </div>
     </div>
   );
